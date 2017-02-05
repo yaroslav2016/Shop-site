37cb2b39a0c47e9023cc9276d6a857d065bf0d72
Yaroslav <yaroslavrokhmanov@gmail.com>
2017-02-05 07:22:02 +0300

    left-sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
-
 $(function(){
 	$('.carousel').carousel({
-		interval:3.0;
+		interval: false
 	});
 
-// $('.search').on('click', function(){
-// 	$('.main-menu .navbar-form').slideToggle();
-// });
+	$('.search').on('click', function(){
+		$('.main-menu .navbar-form').slideToggle();
+	});
+});
+
+$(window).load(function(){
+	var carouselCaptionWidth = $('#carousel-sidebar .active img').width();
+	$('#carousel-sidebar img').each(function(){
+		$(this).attr('width', carouselCaptionWidth);
+	});
+	$('#carousel-sidebar .sidebar-carousel-caption').css('max-width', carouselCaptionWidth + 'px');
+	$('#carousel-sidebar .carousel-indicators').css('max-width', carouselCaptionWidth + 'px');
+	$('.sidebar .carousel .banner').css('max-width', carouselCaptionWidth + 'px');
 });
